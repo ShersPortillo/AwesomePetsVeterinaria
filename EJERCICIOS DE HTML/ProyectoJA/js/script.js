3a1b78c77fc066c15fa43022fe084dacff7d22ce
Shers <153848623+ShersPortillo@users.noreply.github.com>
2024-03-03 15:37:40 -0600

    Delete EJERCICIOS DE HTML/ProyectoJA directory

--- EJERCICIOS DE HTML/ProyectoJA/js/script.js
+++
@@ -1,4 +0,0 @@
-function cambiarTexto() {
-    var infoText = document.getElementById("infoText");
-    infoText.innerHTML = "En AWESOMEPETS, nos dedicamos a proporcionar cuidados excepcionales para tus mascotas. Nuestro equipo apasionado y comprometido está aquí para garantizar la salud y felicidad de tus amigos peludos.";
-}
